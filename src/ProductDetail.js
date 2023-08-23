4422b97449911fe2e697d69eab3ee5f050d47c36
Otto <olarsen22@gmail.com>
2023-08-23 19:39:29 +0200

    add return to productdetail component

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -8,4 +8,32 @@ function ProductDetail() {
   const [product, setProduct] = useState(null);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    // Fetch product details from API
+    fetch(`/api/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, [id]);
+
+  const handleAddToCart = () => {
+    dispatch(addToCart(product));
+  };
+
+  return (
+    <div>
+      {product ? (
+        <>
+          <h2>{product.name}</h2>
+          <img src={product.image} alt={product.name} />
+          <p>{product.description}</p>
+          <p>${product.price}</p>
+          <button onClick={handleAddToCart}>Add to Cart</button>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
   export default ProductDetail;
