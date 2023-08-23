c29f817487d6f62c682d578b1c34dda0445330c0
Otto <olarsen22@gmail.com>
2023-08-23 19:37:07 +0200

    add product logic to productlist component

--- src/ProductList.js
+++ src/ProductList.js
@@ -12,7 +12,17 @@ function ProductList() {
   }, []);
 
   return (
-    
+    <div>
+      <h2>Products</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <Link to={`/product/${product.id}`}>{product.name}</Link>
+            <span>${product.price}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
