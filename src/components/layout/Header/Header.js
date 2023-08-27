863fa438db8e0a77f976aa0dc0a3043379f46f03
Otto <olarsen22@gmail.com>
2023-08-27 22:42:23 +0200

    add background darken pseudo element to Header.css

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -5,9 +5,9 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
+      <div className="background"></div>
       <h1 className="header-title">serenity beyond</h1>
       <div className="header-nav">
-        <input type="text" placeholder="Search" className="search-bar" />
         <nav className="nav-links">
           <Link to="/">Home</Link>
           <Link to="/new-arrivals">New Arrivals</Link>
@@ -15,10 +15,6 @@ function Header() {
           <Link to="/contact">Contact Us</Link>
           <Link to="/cart">Cart</Link>
         </nav>
-        <div className="header-icons">
-          <span className="account-icon">👤</span>
-          <span className="cart-icon">🛒</span>
-        </div>
       </div>
     </header>
   );
