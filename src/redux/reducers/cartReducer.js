9fd5116789e1192c53f612d2c46304957e793670
Otto <olarsen22@gmail.com>
2023-08-27 21:36:48 +0200

    correct import for addToCart file

---
+++ src/redux/reducers/cartReducer.js
@@ -0,0 +1,19 @@
+import { ADD_TO_CART } from '../actions/addToCart'; 
+
+const initialState = {
+  items: [],
+};
+
+const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      };
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
