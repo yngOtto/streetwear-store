00d6d5236500f2f6499e99e6759c7c3a27aa7138
Otto <olarsen22@gmail.com>
2023-08-27 21:51:33 +0200

    add Links and searchbar to Header.js

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css';
+import './Header.css';  
 
 function Header() {
   return (
-    <header>
-      <h1>serenetiy beyond</h1>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/cart">Cart</Link>
-        {/* other links */}
-      </nav>
+    <header className="header">
+      <h1 className="header-title">serenity beyond</h1>
+      <div className="header-nav">
+        <input type="text" placeholder="Search" className="search-bar" />
+        <nav className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/new-arrivals">New Arrivals</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/contact">Contact Us</Link>
+          <Link to="/cart">Cart</Link>
+        </nav>
+        <div className="header-icons">
+          <span className="account-icon">👤</span>
+          <span className="cart-icon">🛒</span>
+        </div>
+      </div>
     </header>
   );
 }
