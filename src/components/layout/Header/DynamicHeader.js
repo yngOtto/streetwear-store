f255e1d30ea27c5aaad0e317dae86700f21e4599
Otto <olarsen22@gmail.com>
2023-08-27 23:15:31 +0200

    adjust padding for DynamicHeader tab

--- src/components/layout/Header/DynamicHeader.js
+++ src/components/layout/Header/DynamicHeader.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './DynamicHeader.css';
 
-const messages = ['transcending wordly troubles', 'attaining a form of equanimity that remains impervious to external perturbations', '50% Off!', 'Limited Time Only!'];
+const messages = ['TRANSCENDING WORLDLY TROUBLES', 'ATTAINING A FORM OF EQUANIMITY THAT REMAINS IMPERVIOUS TO EXTERNAL PERTURBATIONS', '50% OFF!', 'LIMITED TIME ONLY!'];
 
 function DynamicHeader() {
   const [currentMessage, setCurrentMessage] = useState(messages[0]);
