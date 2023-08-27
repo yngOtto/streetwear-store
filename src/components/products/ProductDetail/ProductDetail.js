2f4a93da5229447bdb426cb8b98ed876a4716c4f
Otto <olarsen22@gmail.com>
2023-08-27 18:17:18 +0200

    update import to addToCart file

--- src/components/products/ProductDetail/ProductDetail.js
+++ src/components/products/ProductDetail/ProductDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { addToCart } from '../actions';
+import { addToCart } from '../../../redux/actions/addToCart';
 
 function ProductDetail() {
   const { id } = useParams();
