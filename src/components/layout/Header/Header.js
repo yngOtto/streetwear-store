6d888e42984af98dd9bcdbdd88e4f3e8cab24297
Otto <olarsen22@gmail.com>
2023-09-02 20:34:15 +0200

    update header.css with cart

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -6,7 +6,7 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';  
 
 function Header() {
-
+const cartItems = useSelector((state) => state.cartItems);
 
 
   return (
@@ -21,6 +21,12 @@ function Header() {
           <Link to="/about">about us</Link>
           <Link to="/contact">contact us</Link>
         </nav>
+        <div className="cart-icon">
+          <Link to="/cart">
+            <FontAwesomeIcon icon={faShoppingCart} />
+            <span className="cart-count">{cartItemCount}</span>
+          </Link>
+        </div>
       </div>
     </header>
   );
