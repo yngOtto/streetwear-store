95dea113506e5170b7c9009ccce907329e431f8e
Otto <olarsen22@gmail.com>
2023-09-02 20:24:34 +0200

    add new-arrivals link to header.js

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -10,14 +10,14 @@ function Header() {
   return (
     <header className="header">
       <div className="background"></div>
-      <h1 className="header-title">serenity beyond</h1>
+      <h1 className="header-title">placeholder</h1>
       <div className="header-nav">
         <nav className="nav-links">
           <Link to="/">home</Link>
+          <Link to="/new-arrivals">new arrivals</Link>
           <Link to="/collections">collections</Link>
           <Link to="/about">about us</Link>
           <Link to="/contact">contact us</Link>
-          <Link to="/cart">cart</Link>
         </nav>
       </div>
     </header>
