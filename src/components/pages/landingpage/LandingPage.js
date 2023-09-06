8f8d3351a3def8b114fc11c8596390a97f5034c7
Otto <olarsen22@gmail.com>
2023-09-06 16:20:30 +0200

    remove unnessecary comments

--- src/components/pages/landingpage/LandingPage.js
+++ src/components/pages/landingpage/LandingPage.js
@@ -4,25 +4,25 @@ import './LandingPage.css';
 // import ProductCard from '../products/ProductCard/ProductCard'; // If you have a ProductCard component for individual products
 
 const LandingPage = () => {
-  // You can fetch or hardcode featured products here
+
   
   return (
     <div className="landing-page" style={{ backgroundColor: '#121212', color: 'white' }}>
-      {/* Hero Section */}
+      {/* hero section */}
       <section className="hero-section">
         <h1>FALL_01 
           DROPPING SOON
         </h1>
-        {/* Add more content or images */}
+
       </section>
       
-      {/* Featured Products */}
+      {/* featured products */}
       <section className="new-arrivals">
         <h2>new arrivals .-</h2>
-        {/* Map through your featured products and render ProductCard components */}
+
       </section>
       
-      {/* About Section */}
+      {/* about section */}
       <section className="about-section">
         <h2>About Us</h2>
         <p>Define your style with our unique and timeless streetwear.</p>
