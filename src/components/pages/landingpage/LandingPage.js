7e88649efe654ee10af70ab4d2bee4871d6278a2
Otto <olarsen22@gmail.com>
2023-09-06 16:11:38 +0200

    update header and landing page hero background

--- src/components/pages/landingpage/LandingPage.js
+++ src/components/pages/landingpage/LandingPage.js
@@ -10,7 +10,7 @@ const LandingPage = () => {
     <div className="landing-page" style={{ backgroundColor: '#121212', color: 'white' }}>
       {/* Hero Section */}
       <section className="hero-section">
-
+        <h1>serenity beyond</h1>
         {/* Add more content or images */}
       </section>
       
